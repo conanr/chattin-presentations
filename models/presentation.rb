5cf045fb98af2f163b1b32585291d4757a4a15c5
Elise Worthy <elise.worthy@livingsocial.com>
2012-06-10 14:57:31 -0400

    presentation owner after create of presentation. next up, do error handling

--- models/presentation.rb
+++ models/presentation.rb
@@ -3,4 +3,10 @@ class Presentation < ActiveRecord::Base
   validates :deck_url, presence: true
   validates_presence_of :user_id
   has_many :presentation_owners
+
+  after_create :create_ownership
+
+  def create_ownership
+    PresentationOwner.create({presentation_id: self.id,user_id: self.user_id})
+  end
 end
--- models/presentation_owner.rb
+++ models/presentation_owner.rb
@@ -1,4 +1,4 @@
-class Presentation < ActiveRecord::Base
+class PresentationOwner < ActiveRecord::Base
   validates :user_id, :presentation_id, presence: true
   belongs_to :presentation
 end
